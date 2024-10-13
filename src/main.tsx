7d75333bc1d95ed72a42de0fde88e271f435f7fd
Zaur Yusufov <zaury8252@gmail.com>
2024-10-13 18:14:02 +0400

    Fix routing and deployment configuration for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { MantineProvider } from '@mantine/core'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
 import MainPage from './page/mainPage/MainPage.tsx'
 import '@mantine/core/styles.css'
 import './index.css'
 
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <MainPage/>,
-    },
-    {
-        path: '/explore',
-        element: <div/>,
-    },
-])
+const router = createHashRouter(
+    [
+        { path: '/', element: <MainPage /> },
+        { path: '/explore', element: <div /> },
+    ],
+    { basename: '/business-card' }
+)
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <MantineProvider>
             <RouterProvider router={router} />
         </MantineProvider>
-    </StrictMode>,
+    </StrictMode>
 )
